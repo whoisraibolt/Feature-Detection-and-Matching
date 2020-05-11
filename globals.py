a26a0265132fe6bb2fb6b11f28443a304decdedf
Alexandra Raibolt <alexandra.raibolt@gmail.com>
2020-05-11 14:56:44 -0300

    Initial commit globals.py

---
+++ globals.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+def initialize():
+	global descriptor
+	global descriptors1
+	global descriptors2
+	global detector
+	global keypoints1
+	global keypoints2
+	global outputs
