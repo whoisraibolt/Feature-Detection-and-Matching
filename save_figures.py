05bce4d93ff6e93d8e444b0e44eaf1ddf1e54e1d
Alexandra Raibolt <alexandra.raibolt@gmail.com>
2020-05-11 14:57:38 -0300

    Initial commit save_figures.py

---
+++ save_figures.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Imports
+from matplotlib import pyplot as plt
+
+# Call function saveMatcher
+def saveMatcher(output,
+			    matcher,
+                descriptor):
+	# Plot BFMatcher with ORB
+
+	# Turn interactive plotting off
+	plt.ioff()
+
+	# Create a new figure
+	plt.figure()
+	plt.axis('off')
+	plt.imshow(output)
+
+	plt.imsave(fname = 'Figures/%s-with-%s.png' % (matcher, descriptor),
+			arr = output,
+			dpi = 300)
+
+	# Close it
+	plt.close()
